eeeaeaccfe1da64df50bfa216b0be80584f89ae4
Duga Emmanuel <dugaemmanuel846@gmail.com>
2026-01-28 14:56:27 +0000

    implmenting synyar print

--- android/src/main/java/com/it4u/telpo/com/TelpoPrintPlugin.java
+++ android/src/main/java/com/it4u/telpo/com/TelpoPrintPlugin.java
@@ -8,10 +8,16 @@ import com.getcapacitor.annotation.CapacitorPlugin;
 import com.telpo.tps550.api.TelpoException;
 import com.telpo.tps550.api.printer.ThermalPrinter;
 import com.telpo.tps550.api.printer.UsbThermalPrinter;
+import android.os.Build;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import com.sunyard.api.printer.IPrinter;
+import com.sunyard.api.printer.OnPrintListener;
+import com.sunyard.api.printer.PrintConstant;
+
 
 @CapacitorPlugin(name = "TelpoPrint")
 public class TelpoPrintPlugin extends Plugin {
@@ -33,7 +39,12 @@ public class TelpoPrintPlugin extends Plugin {
         try {
             printer.setMonoSpace(true);
             printer.setGray(7);
-            printReceipt(data);
+            if(isSunyard()){
+                printSunYard(new JSONObject(data.toString()),this.getContext());
+            }else{
+                printReceipt(data);
+            }
+
         }catch (TelpoException t){
             String result = t.toString();
             if (result.equals("com.telpo.tps550.api.printer.NoPaperException")) {
@@ -43,6 +54,11 @@ public class TelpoPrintPlugin extends Plugin {
             }
         }
         call.resolve();
+    }
+     public boolean isSunyard(){
+        String manufacturer = Build.MANUFACTURER.toLowerCase();
+        String model = Build.MODEL.toLowerCase();
+        return manufacturer.contains("sunyard") || model.contains("s200");
     }
     void printReceipt(JSONObject data){
         try {
@@ -89,6 +105,102 @@ public class TelpoPrintPlugin extends Plugin {
             }
         }catch (TelpoException telpoException){
             telpoException.printStackTrace();
+        }
+    }
+    void printSunYard(JSONObject data,Context context){
+        try {
+            IPrinter printer = DeviceServiceGet.getInstance().getPrinter();
+
+            if (printer == null) {
+                presentToast(context, "Erreur: Imprimante non disponible", ToastType.LONG);
+                return;
+            }
+
+            // Configuration
+            printer.setGray(5);
+
+            // Bundle pour le texte
+            android.os.Bundle textCenterBundle = new android.os.Bundle();
+            android.os.Bundle textLeftBundle = new android.os.Bundle();
+            android.os.Bundle textRightBundle = new android.os.Bundle();
+
+            textCenterBundle.putInt("align", PrintConstant.Align.CENTER);
+            textCenterBundle.putInt("font", PrintConstant.FontSize.NORMAL);
+            textCenterBundle.putInt("fontTemplate", PrintConstant.FontTemplate.DEFAULT);
+
+            textLeftBundle.putInt("align", PrintConstant.Align.LEFT);
+            textLeftBundle.putInt("font", PrintConstant.FontSize.NORMAL);
+            textLeftBundle.putInt("fontTemplate", PrintConstant.FontTemplate.DEFAULT);
+
+            textRightBundle.putInt("align", PrintConstant.Align.RIGHT);
+            textRightBundle.putInt("font", PrintConstant.FontSize.NORMAL);
+            textRightBundle.putInt("fontTemplate", PrintConstant.FontTemplate.DEFAULT);
+
+            // 1. Imprimer le logo
+            // try {
+            //     InputStream inputStream = context.getAssets().open("bcc-logo.bmp");
+            //     Bitmap logoBitmap = BitmapFactory.decodeStream(inputStream);
+
+            //     android.os.Bundle logoBundle = new android.os.Bundle();
+            //     logoBundle.putInt("offset", 72); // Centrer le logo
+
+            //     printer.addImage(logoBundle, bitmapToByteArray(logoBitmap));
+            //     inputStream.close();
+            // } catch (IOException e) {
+            //     // Continuer sans logo si erreur
+            //     e.printStackTrace();
+            // }
+            //entêtes
+             String title = data.getString("title");
+                String footer = data.getString("footer");
+                JSONArray lines = data.getJSONArray("lines");
+                JSONObject header = data.getJSONObject("header");
+                String agencyName = header.getString("agencyName");
+                String agencyContact = header.getString("agencyContact");
+                String agencyAdress = header.getString("agencyAdress");
+                printer.addText(textCenterBundle, agencyName +"\n");
+                printer.addText(textCenterBundle, "Tel: "+agencyContact);
+                printer.addText(textCenterBundle, "Adresse: "+agencyAdress );
+                printer.addText(textCenterBundle, title );
+                printer.feedLine(5);
+                
+            // 2. Imprimer les lignes de texte
+            for (int i = 0; i < lines.length(); i++) {
+               JSONObject val = lines.getJSONObject(i);
+                    String key = val.getString("title");
+                    String value = val.getString("value");
+                    printer.addText(textLeftBundle, key + ": " + value + "\n");
+            }
+
+            // 3. Imprimer le QR code
+            // Bitmap qrCode = generateQRCodeBitmap(qrString);
+            // if (qrCode != null) {
+            //     android.os.Bundle qrBundle = new android.os.Bundle();
+            //     qrBundle.putInt("align", PrintConstant.Align.CENTER);
+            //     qrBundle.putInt("expectedHeight", 200);
+
+            //     printer.addQrCode(qrBundle, qrString);
+            // }
+
+            // 4. Ajouter des lignes vides à la fin
+            printer.feedLine(10);
+
+            // 5. Lancer l'impression
+            printer.startPrint(new OnPrintListener.Stub() {
+                @Override
+                public void onFinish() throws RemoteException {
+                    // Impression réussie
+                }
+
+                @Override
+                public void onError(int errorCode) throws RemoteException {
+                    presentToast(context, "Erreur d'impression: " + errorCode, ToastType.LONG);
+                }
+            });
+
+        } catch (RemoteException e) {
+            e.printStackTrace();
+            presentToast(context, "Erreur: " + e.getMessage(), ToastType.LONG);
         }
     }
 }
